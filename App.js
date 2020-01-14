a41b5789f59b679c908604d24590ab363e39033b
rabiei <rabiei@jetsgram.ir>
2020-01-14 23:45:27 +0330

    Using InlineStyling

--- App.js
+++ App.js
@@ -4,8 +4,8 @@ import { StyleSheet, Text, View ,TextInput, Button} from 'react-native';
 export default function App() {
   return (
     <View style={{padding:40}}>
-    <View>
-    <TextInput placeholder="course goal" style={{borderColor:'black', borderWidth:1,padding:10}}/>
+    <View style={{flexDirection:'row', justifyContent: 'space-between', alignItems:'center'}}>
+    <TextInput placeholder="course goal" style={{width:'80%', borderColor:'black', borderWidth:1,padding:10}}/>
     <Button title="Add" />
     </View>
     <View></View>
