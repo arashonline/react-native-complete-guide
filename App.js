b3d6c530f52c44648f037a092a6f82102b142994
rabiei <rabiei@jetsgram.ir>
2020-01-14 23:39:43 +0330

    adding TextInput and Button adding some style adding placeholder to TextInput

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View ,TextInput, Button} from 'react-native';
 
 export default function App() {
   return (
-    <View >
-    <View></View>
+    <View style={{padding:40}}>
+    <View>
+    <TextInput placeholder="course goal" style={{borderColor:'black', borderWidth:1,padding:10}}/>
+    <Button title="Add" />
+    </View>
     <View></View>
     </View>
   );
