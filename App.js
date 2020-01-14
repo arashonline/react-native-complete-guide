e1706fcfd8e6e0afbe27a9eec0fe84d42cb1bd1b
rabiei <rabiei@jetsgram.ir>
2020-01-15 00:14:13 +0330

    short-cut to flex

--- App.js
+++ App.js
@@ -1,17 +1,44 @@
 import React from 'react';
-import { StyleSheet, Text, View ,TextInput, Button} from 'react-native';
+import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={{padding:40}}>
-    <View style={{flexDirection:'row', justifyContent: 'space-between', alignItems:'center'}}>
-    <TextInput placeholder="course goal" style={{width:'80%', borderColor:'black', borderWidth:1,padding:10}}/>
-    <Button title="Add" />
-    </View>
-    <View></View>
+    <View style={{ padding: 20,
+       flexDirection: 'row',
+        width: '100%',
+        height: 300,
+         justifyContent: 'space-around',
+          alignItems: 'stretch' }}>
+      <View
+        style={{
+          backgroundColor: 'red',
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+      >
+        <Text>1</Text>
+      </View>
+      <View
+        style={{
+          backgroundColor: 'blue',
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+      >
+        <Text>2</Text>
+      </View>
+      <View
+        style={{
+          backgroundColor: 'green',
+          flex: 2,
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+      >
+        <Text>3</Text>
+      </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-});
