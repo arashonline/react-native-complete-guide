094cc4209f441b3aa67342fd7d87972f0b9abf86
rabiei <rabiei@jetsgram.ir>
2020-01-15 00:45:41 +0330

    some listStyling

--- App.js
+++ App.js
@@ -1,44 +1,57 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, {useState} from "react";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal]= useState('');
+  const [courseGoals, setCourseGoals] = useState([])
+
+  const goalInputHandler = (enteredText)=>{
+    setEnteredGoal(enteredText);
+  };
+
+  const addGoalHandler = ()=>{
+    setCourseGoals(currentGoals=>[...courseGoals, enteredGoal]);
+  }
+
   return (
-    <View style={{ padding: 20,
-       flexDirection: 'row',
-        width: '100%',
-        height: 300,
-         justifyContent: 'space-around',
-          alignItems: 'stretch' }}>
-      <View
-        style={{
-          backgroundColor: 'red',
-          flex: 1,
-          justifyContent: 'center',
-          alignItems: 'center'
-        }}
-      >
-        <Text>1</Text>
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="course goal"
+          style={styles.input}
+          onChangeText={goalInputHandler}
+          value={enteredGoal}
+        />
+        <Button title="Add" onPress={addGoalHandler}/>
       </View>
-      <View
-        style={{
-          backgroundColor: 'blue',
-          flex: 1,
-          justifyContent: 'center',
-          alignItems: 'center'
-        }}
-      >
-        <Text>2</Text>
-      </View>
-      <View
-        style={{
-          backgroundColor: 'green',
-          flex: 2,
-          justifyContent: 'center',
-          alignItems: 'center'
-        }}
-      >
-        <Text>3</Text>
+      <View>
+        {courseGoals.map((goal)=><View  key={goal} style={styles.listItem}><Text>{goal}</Text></View>)}
       </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  screen: {
+    padding: 40
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center"
+  },
+  input: {
+    width: "80%",
+    borderColor: "black",
+    borderWidth: 1,
+    padding: 10
+  },
+  listItem:{
+    padding:10,
+    marginTop:10,
+    backgroundColor: '#ccc',
+    borderColor:'black',
+    borderWidth:1,
+    
+  }
+});
