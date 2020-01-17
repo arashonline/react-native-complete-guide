cb924d772b1fe349d3faa81dd31baad05288ee01
rabiei <rabiei@jetsgram.ir>
2020-01-17 19:46:31 +0330

    improving styles

--- App.js
+++ App.js
@@ -15,9 +15,10 @@ import GoalInput from "./components/GoalInput";
 export default function App() {
   const [courseGoals, setCourseGoals] = useState([]);
 
-  const [isAddMode,setIsAddMode] = useState(false)
+  const [isAddMode,setIsAddMode] = useState(false);
 
   const addGoalHandler = goalTitle => {
+    setIsAddMode(false);
     setCourseGoals(currentGoals => [
       ...courseGoals,
       { id: Math.random().toString(), value: goalTitle }
@@ -30,19 +31,22 @@ export default function App() {
     });
   };
 
+  const cancelGoalAdditionPopup = ()=>{
+    setIsAddMode(false);
+  }
   
 
   return (
     <View style={styles.screen}>
     <Button title="Add New Goal" onPress={()=>setIsAddMode(true)}/>
-      <GoalInput onAddGoal={addGoalHandler} isVisible={isAddMode}/>
+      <GoalInput onAddGoal={addGoalHandler} isVisible={isAddMode} onCancel={cancelGoalAdditionPopup}/>
       <FlatList
         keyExtractor={(item, index) => item.id}
         data={courseGoals}
         renderItem={itemData => (
           <GoalItem
             id={itemData.item.id}
-            onDelete={removeGoalHandler}
+            onDelete={removeGoalHandler}            
             title={itemData.item.value}
           />
         )}
