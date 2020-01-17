5ef9c0339d03e394cf1ad396ecfbb38ebf0660e0
rabiei <rabiei@jetsgram.ir>
2020-01-17 23:47:19 +0330

    some minor improvement

--- App.js
+++ App.js
@@ -18,6 +18,9 @@ export default function App() {
   const [isAddMode,setIsAddMode] = useState(false);
 
   const addGoalHandler = goalTitle => {
+    if(goalTitle.length === 0){
+      return;
+    }
     setIsAddMode(false);
     setCourseGoals(currentGoals => [
       ...courseGoals,
