1efbfc6f02de55e6c9ebe7250676171e25ad168c
rabiei <rabiei@jetsgram.ir>
2020-01-18 00:05:22 +0330

    init-clean for creating gusseing game

--- App.js
+++ App.js
@@ -2,58 +2,16 @@ import React, { useState } from "react";
 import {
   StyleSheet,
   Text,
-  View,
-  TextInput,
-  Button,
-  ScrollView,
-  FlatList
+  View
 } from "react-native";
 
-import GoalItem from "./components/GoalItem";
-import GoalInput from "./components/GoalInput";
 
 export default function App() {
-  const [courseGoals, setCourseGoals] = useState([]);
-
-  const [isAddMode,setIsAddMode] = useState(false);
-
-  const addGoalHandler = goalTitle => {
-    if(goalTitle.length === 0){
-      return;
-    }
-    setIsAddMode(false);
-    setCourseGoals(currentGoals => [
-      ...courseGoals,
-      { id: Math.random().toString(), value: goalTitle }
-    ]);
-  };
-
-  const removeGoalHandler = goalId => {
-    setCourseGoals(currentGoals => {
-      return currentGoals.filter(goal => goal.id !== goalId);
-    });
-  };
-
-  const cancelGoalAdditionPopup = ()=>{
-    setIsAddMode(false);
-  }
-  
+ 
 
   return (
     <View style={styles.screen}>
-    <Button title="Add New Goal" onPress={()=>setIsAddMode(true)}/>
-      <GoalInput onAddGoal={addGoalHandler} isVisible={isAddMode} onCancel={cancelGoalAdditionPopup}/>
-      <FlatList
-        keyExtractor={(item, index) => item.id}
-        data={courseGoals}
-        renderItem={itemData => (
-          <GoalItem
-            id={itemData.item.id}
-            onDelete={removeGoalHandler}            
-            title={itemData.item.value}
-          />
-        )}
-      />
+    <Text>START THE GAME</Text>
     </View>
   );
 }
