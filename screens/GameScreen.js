c774725af71c25e33cb6bc9cb60dfe3f1c36e9f3
rabiei <rabiei@jetsgram.ir>
2020-01-19 00:04:38 +0330

    Styling GameScreen selecting screeng conditionally

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,5 +1,8 @@
 import React, {useState} from 'react';
-import { View,Text,StyleSheet } from 'react-native';
+import { View,Text,StyleSheet, Button } from 'react-native';
+
+import NumberContainer from "../components/NumberContainer";
+import Card from "../components/Card";
 
 const generateRandomBetween = (min,max,exclude)=>{
     min = Math.ceil(min);
@@ -16,12 +19,50 @@ const generateRandomBetween = (min,max,exclude)=>{
 const GameScreen = props =>{
     const [currentGuess,setCurrentGuess]= useState(generateRandomBetween(1,100,props.userChoice))
     return (
-        <View></View>
+        <View style={styles.screen}>
+        <Text>Computer Guess:</Text>
+        <NumberContainer>{currentGuess}</NumberContainer>
+        <Card style={styles.buttonContainer}>
+        <Button title="Lower" onPress={()=>{}}/>
+        <Button title="Higher" onPress={()=>{}}/>
+        </Card>
+        </View>
         );
 }
 
-const style = StyleSheet.create({
 
-});
+const styles = StyleSheet.create({
+    screen: {
+      flex: 1,
+      padding: 10,
+      alignItems: 'center'
+    },
+    buttonContainer: {
+      flexDirection: 'row',
+      justifyContent: 'space-around',
+      marginTop: 20,
+      width: 400,
+      maxWidth: '90%'
+    },
+    listContainer: {
+      flex: 1,
+      width: '60%'
+    },
+    list: {
+      flexGrow: 1,
+      // alignItems: 'center',
+      justifyContent: 'flex-end',
+    },
+    listItem: {
+      borderColor: '#ccc',
+      borderWidth: 1,
+      padding: 15,
+      marginVertical: 10,
+      backgroundColor: 'white',
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      width: '100%'
+    }
+  });
 
 export default GameScreen;
