13ff46954f41d4a0d55c79e311a0f328ac8e98e8
rabiei <rabiei@jetsgram.ir>
2020-01-19 23:37:35 +0330

    Using web images

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -11,6 +11,11 @@ const GameOverScreen = props => {
             <View style={styles.imageContainer}>
             <Image style={styles.image} source={require('../assets/success.jpg')} resizeMode="cover" />
             </View>
+            <View style={styles.imageContainer}>
+            <Image style={styles.image} fadeDuration={1000} source={{
+                uri:"https://previews.123rf.com/images/dizanna/dizanna1602/dizanna160202120/52562960-succeed-word-cloud-business-concept.jpg"
+            }} resizeMode="cover" />
+            </View>
             <BodyText>Number Of Rounds: {props.roundsNumber}</BodyText>
             <BodyText>Number Was: {props.userNumber}</BodyText>
             <Button title="NEW GAME" onPress={props.onNewGame} />
@@ -29,9 +34,9 @@ const styles = StyleSheet.create({
         height: '100%',
     },
     imageContainer:{
-        width:'80%',
-        height:300,
-        borderRadius:200,
+        width:100,
+        height:100,
+        borderRadius:50,
         borderWidth:3,
         borderColor: 'black',
         overflow:'hidden'
